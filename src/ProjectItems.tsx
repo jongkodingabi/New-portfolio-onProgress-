5e3c96c7d4224f2a59aee2eb7764abbb4201c436
jongkodingabi <abisamhazim04@gmail.com>
2025-04-06 23:15:06 +0700

    optimizing projects

---
+++ src/ProjectItems.tsx
@@ -0,0 +1,60 @@
+import YourMeals from "@/assets/projects/YourMeals.png";
+import BeedeGo from "@/assets/projects/BeedeGo.png";
+import KataKita from "@/assets/projects/kataKita.png";
+import Caseths from "@/assets/projects/caseth-la-magasin_web.jpg";
+import TodoApp from "@/assets/projects/To-Do List App.png";
+
+const itemsProjects = [
+  {
+    id: 1,
+    image: YourMeals,
+    title: "Your Meals",
+    description:
+      "A web application that allows users to search for recipes and save their favorite meals.",
+    tags: ["Vue.js", "Tailwind CSS", "MealDB API"],
+    link: "https://your-meals-app.netlify.app/",
+    cta: "View site",
+  },
+  {
+    id: 2,
+    image: BeedeGo,
+    title: "BeedeGo",
+    description:
+      "A simple landing page for a design service, showcasing their projects and services.",
+    tags: ["Vue.js", "Tailwind CSS"],
+    link: "https://beedego-abisam-hazims-projects.vercel.app/",
+    cta: "View site",
+  },
+  {
+    id: 3,
+    image: KataKita,
+    title: "KataKita",
+    description:
+      "An Indonesian dictionary application made with Flutter and Firebase",
+    tags: ["Flutter", "Firebase"],
+    link: "https://github.com/jongkodingabi/KataKita",
+    cta: "View github",
+  },
+  {
+    id: 4,
+    image: Caseths,
+    title: "Caseths",
+    description:
+      "Website that contains product information in the form of accessories, cart and payment gatheway",
+    tags: ["Laravel", "Tailwind CSS", "MySQL"],
+    link: "https://github.com/jongkodingabi/KataKita",
+    cta: "View github",
+  },
+  {
+    id: 5,
+    image: TodoApp,
+    title: "To-Do App",
+    description:
+      "A to do list app that can save your work notes, I did this project to practice my vue skills",
+    tags: ["Vue.js", "CSS3"],
+    link: "https://github.com/jongkodingabi/KataKita",
+    cta: "View github",
+  },
+];
+
+export default itemsProjects;
--- src/components/reactbits/MainCard.tsx
+++ src/components/reactbits/MainCard.tsx
@@ -1,2 +1,55 @@
-type MainCard = {};
-export default function MainCard() {}
+type MainCardProps = {
+  img: string;
+  title: string;
+  description: string;
+  tags: string[];
+  link: string;
+  cta: string;
+};
+export default function MainCard({
+  img,
+  title,
+  description,
+  tags,
+  link,
+  cta,
+}: MainCardProps) {
+  return (
+    <div className="group shadow-2xl transition-all duration-300 ease-in-out rounded-xl bg-transparent border border-white relative mx-2 z-10 mt-10 hover:scale-105 hover:shadow-lg hover:shadow-gray-600 w-full sm:w-80 md:w-96">
+      <div className="flex flex-col space-y-2 md:space-y-0">
+        <div className="h-full w-full self-stretch flex items-center rounded-t-lg overflow-hidden">
+          <img
+            src={img}
+            alt="Project Image"
+            className="h-full w-full object-contain group-hover:scale-110 group-hover:rotate-2 transition-transform duration-300"
+          />
+        </div>
+        <div className="p-4 md:p-4 mt-2 space-y-2">
+          <h1 className="text-sm md:text-lg lg:text-lg font-bold text-white transition-all ease-in-out text-left mb-5">
+            {title}
+          </h1>
+          <p className="text-xs md:text-base font-semibold text-gray-300 mt-1 text-left">
+            {description}
+          </p>
+        </div>
+        <div className="flex flex-wrap pl-4 mb-4 gap-2">
+          {tags.map((tag, index) => (
+            <span
+              key={index}
+              className="cursor-default px-4 py-1 rounded-full bg-transparent hover:bg-gray-600 transition-all duration-300 border border-white text-xs md:text-sm font-semibold text-gray-300"
+            >
+              {tag}
+            </span>
+          ))}
+        </div>
+        <div className="flex flex-row p-2 mb-4">
+          <a href={link} target="_blank" rel="noopener noreferrer">
+            <button className="cursor-pointer w-full sm:w-auto px-4 py-1 text-lg font-bold bg-white rounded-full text-black hover:bg-gray-200 transition-all duration-300 border border-white">
+              {cta}
+            </button>
+          </a>
+        </div>
+      </div>
+    </div>
+  );
+}
