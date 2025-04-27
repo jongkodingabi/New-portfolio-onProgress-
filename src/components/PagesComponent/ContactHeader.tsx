2a20eb1b47a00d1d3be46b48f0445241a81d3388
jongkodingabi <abisamhazim04@gmail.com>
2025-04-27 17:55:20 +0700

    adding responsive in contact header

--- src/components/PagesComponent/ContactHeader.tsx
+++ src/components/PagesComponent/ContactHeader.tsx
@@ -1,27 +1,14 @@
 export default function Quote() {
   return (
-    <div className="bg-black w-screen flex flex-col items-center justify-center text-center">
+    <div className="bg-black w-full flex flex-col items-center justify-center text-center overflow-x-hidden">
       {/* title */}
-      <span className="mx-auto flex items-center justify-center z-0 absolute whitespace-nowrap text-center tracking-tighter text-4xl md:text-7xl lg:text-[100px] font-extrabold text-white opacity-10 uppercase lg:mt-30 mt-2">
+      <span className="mx-auto flex items-center justify-center z-0 absolute whitespace-nowrap text-center tracking-tighter text-2xl md:text-7xl lg:text-[100px] font-extrabold text-white opacity-10 uppercase lg:mt-30 mt-2">
         <span className="">
           <h1 className="">CONTACT</h1>
         </span>
       </span>
 
-      <div className="py-7 lg:mt-40 mt-20" data-aos="fade-up">
-        {/* Quote Icon */}
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          width="24"
-          height="24"
-          viewBox="0 0 24 24"
-          fill="currentColor"
-          className="text-white w-20 h-20 mx-auto mb-4"
-          stroke="currentColor"
-        >
-          <path d="M9.983 3v7.391c0 5.704-3.731 9.57-8.983 10.609l-.995-2.151c2.432-.917 3.995-3.638 3.995-5.849h-4v-10h9.983zm14.017 0v7.391c0 5.704-3.748 9.571-9 10.609l-.996-2.151c2.433-.917 3.996-3.638 3.996-5.849h-3.983v-10h9.983z" />
-        </svg>
-
+      <div className="py-7 lg:mt-80 mt-25" data-aos="fade-up">
         {/* Qoute */}
         <h3
           className="text-2xl md:text-6xl mx-8 md:mx-20 lg:mx-60 lg:text-7xl tracking-tighter font-extrabold font-impact text-white text-center"
@@ -29,9 +16,19 @@ export default function Quote() {
         >
           I'D LOVE TO HEAR FROM YOU
         </h3>
+        <h3
+          className="text-2xl md:text-6xl mx-8 md:mx-20 lg:mx-60 lg:text-7xl tracking-tighter font-extrabold font-impact text-gray-400 text-center"
+          data-aos="fade-up"
+        >
+          CONTACT ME
+        </h3>
 
         {/* Author */}
-        <p className="text-lg font-light text-gray-400 mt-4">Abisam Hazim</p>
+        <p className="text-xs md:text-2xl font-bold text-gray-400 mt-4 mx-4 md:mx-40">
+          Let's turn ideas into reality! If you have a project in mind, need
+          assistance, or simply want to start a conversation, feel free to drop
+          me a message anytime.
+        </p>
       </div>
     </div>
   );
