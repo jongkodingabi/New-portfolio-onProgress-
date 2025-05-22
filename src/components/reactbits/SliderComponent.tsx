71ef5b3bbddcf7dde49f15b1609a47a7f802acb3
jongkodingabi <abisamhazim04@gmail.com>
2025-05-22 17:34:17 +0700

    changing slider

--- src/components/reactbits/SliderComponent.tsx
+++ src/components/reactbits/SliderComponent.tsx
@@ -1,26 +1,29 @@
 import itemsIcons from "@/IconsItems";
-import Slider from "react-infinite-logo-slider";
+// import Slider from "react-infinite-logo-slider";
 
 export default function SliderComponent() {
   return (
-    <div className="slider-wrap">
-      <Slider
-        width="250px"
-        duration={20}
-        pauseOnHover={true}
-        blurBorders={false}
-        blurBorderColor={"#fff"}
-      >
+    <div className="slider-right">
+      <div className="slider-track-right">
         {itemsIcons.map((item) => (
-          <Slider.Slide key={item.id}>
+          <div className="slide">
             <img
               src={item.src}
-              alt={item.alt}
-              className="w-45 md:w-55 object-contain hover:scale-110 transition-all duration-300 grayscale hover:grayscale-0"
+              alt={item.src}
+              className="w-45 md:w-60 object-contain hover:scale-110 transition-all duration-300 grayscale hover:grayscale-0"
             />
-          </Slider.Slide>
+          </div>
         ))}
-      </Slider>
+        {itemsIcons.map((item) => (
+          <div className="slide">
+            <img
+              src={item.src}
+              alt={item.src}
+              className="w-45 md:w-60 object-contain hover:scale-110 transition-all duration-300 grayscale hover:grayscale-0"
+            />
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
